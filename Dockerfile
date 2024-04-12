87bb7b3b378f640e672ddf911a85153d83e66cf1
Sacha Guenoun <sacha.guenoun@efrei.net>
2024-04-12 08:08:33 +0200

    Push end of TP1

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM node:alpine3.19
 WORKDIR /usr/src/app
 COPY package*.json ./
-RUN npm install
+RUN npm install -g npm@10.5.2 && npm cache clean --force
+RUN npm install axios
+RUN apk update && apk upgrade && apk add --no-cache openssl
 COPY . .
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const getWeather = require('./weather');
-require('dotenv').config();
 
-getWeather(process.env.LAT, process.env.LONG, process.env.API_KEY)
-  .then(data => console.log(data))
+const apiKey = process.env.API_KEY;
+const latitude = process.env.LAT;
+const longitude = process.env.LONG;
+
+if (!apiKey || !latitude || !longitude) {
+    console.error("Please provide LAT, LONG, and API_KEY environment variables.");
+    process.exit(1);
+}
+
+getWeather(latitude, longitude, apiKey)
+.then(data => {
+
+  const country = data.sys.country;
+  const weatherMain = data.weather[0].main;
+  const tempMin = (data.main.temp_min - 273.15).toFixed(2); // From Kelvin to Celcius
+  const tempMax = (data.main.temp_max - 273.15).toFixed(2);
+  const description = data.weather[0].description;
+
+  const message = `The current weather in ${data.name}, ${country} (Latitude: ${latitude}, Longitude: ${longitude}) is characterized by ${description}. The temperature is fluctuating between ${tempMin}°C and ${tempMax}°C.`;
+
+  console.log(message);
+})
   .catch(error => console.error(error));
